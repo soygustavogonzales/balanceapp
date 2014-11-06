c06003f87751cb852848751e2b1620494ca82e17
gustavo gonzales <gustavo.gonda@gmail.com>
2014-11-06 01:37:32 -0500

    agregado el modelo de manejo de BD modelDispatcher.js con controladores a tabla despachador

---
+++ model/divercity/modelDispatcher.js
@@ -0,0 +1,60 @@
+//console.log(__dirname)//MUY UTIL PARA VER LA RUTA RELATIVA
+var connection = require(__dirname+'/divercity.connection.js');
+var l = console.log;
+var modelDispatcher = new Object();
+
+modelDispatcher.getAllDispatchers = function(opt){
+		if(connection){
+			connection.query("SELECT * FROM despachador",function(err,rows){
+				if(err)	
+					l(err.message);
+				else
+					opt.callback(rows);//arroja un array con las tuplas en formato JSON
+			})
+		}
+}
+
+
+modelDispatcher.getPassword = function(opt){
+	if(connection){
+		var query = ('SELECT pasDes FROM despachador WHERE UPPER(corDes) = UPPER("hsotomayor@gmail.com")');
+			connection.query(query,function(err,rows){
+				if(err){
+					l(err.message);
+				}
+				else {
+					//l(rows);
+					opt.callback(rows);
+				}
+			})
+	
+	}
+}
+
+modelDispatcher.isUserExists = function(opt){
+	if(connection){
+					//l("***");
+		//l(connection.escape(opt.email))
+		var query = ('SELECT idDes FROM despachador WHERE UPPER(corDes) = UPPER("hsotomayor@gmail.com")');
+			connection.query(query,function(err,rows){
+					//l("&&&");
+				if(err){
+					//l("<<");
+					l(err.message);
+					opt.callback(false)
+					throw err
+				}
+				else {
+					//l(">>");
+					//l(rows);
+					if(rows.length>0)
+						opt.callback(true);
+					else
+						opt.callback(false);
+				}	
+			})
+	}
+}
+
+
+module.exports =  modelDispatcher;
